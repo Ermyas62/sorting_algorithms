6e38ef5a087492d8a742295a3a94d4fcdac593ba
Ermyas62 <ermyas62@gmail.com>
2024-01-22 11:49:39 -0800

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -36,15 +36,19 @@ void counting_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 	for (i = 0; i < size; i++)
+	{
 		if (array[i] > maximum)
+		{
 			maximum = array[i];
-	counter = _calloc(maximum + 1, sizeof(int));
+		}
+	}
+	counter = _calloc((maximum + 1), sizeof(int));
 	tmp = _calloc(size + 1, sizeof(int));
 	for (i = 0; i < size; i++)
 		counter[array[i]]++;
 	for (index = 1; index <= maximum; index++)
 		counter[index] += counter[index - 1];
-	print_array(counter, maximum + 1);
+	print_array(counter, (maximum + 1));
 	for (i = 0; i < size; ++i)
 	{
 		tmp[counter[array[i]] - 1] = array[i];
